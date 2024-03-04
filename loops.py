036836ef58c690d23e62d9b8736981d2bf752c22
Wangari-j <wangarijudy@gmail.com>
2024-03-04 07:01:37 +0300

    loops

---
+++ loops.py
@@ -0,0 +1,9 @@
+names = ["Ann", "Angie", "Aley"]
+
+#i - iteration. Thou can use different letter/word
+for i in names:
+    print( i + ",how old are you?")
+
+print("I think they are above 18 years.") #not repeated because its outside the body of the for loop.
+
+    
